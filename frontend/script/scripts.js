5c9f2e1b93572355bd1767140b15f57c42b6e1da
justin <justin.mottier@gmail.com>
2021-03-10 17:44:44 +0100

    can't stop me now

--- frontend/script/scripts.js
+++ frontend/script/scripts.js
@@ -1,35 +1,54 @@
 import ApiWrapper from '../src/ApiWrapper.js'
 
-let a = new ApiWrapper()
-let response = a.getAllPdfFiles()
 
-console.log(response)
+class ViewController {
 
-let fileGallery = document.getElementById('fileGallery')
+    pdfFilesList
 
-function createPdfElement(fileName) {
-    let e = document.createElement('div')
+    constructor() {
+        let a = new ApiWrapper()
+        this.pdfFilesList = a.getAllPdfFiles()
+        if (this.pdfFilesList !== null) {
+            this.hideSpinner()
+            this.displayContent()
+        }
+    }
 
-    let card = document.createElement('div')
-    card.classList.add('uk-card')
-    card.classList.add('uk-card-default')
+    displayContent() {
+        let fileGallery = document.getElementById('fileGallery')
 
-    let icon = document.createElement('span')
-    icon.classList.add('uk-card-body')
-    icon.setAttribute('uk-icon', 'icon: file-pdf; ratio: 8')
+        for (let file of this.pdfFilesList) {
+            console.log(file)
+            fileGallery.append(this.createPdfElement(file['name']))
+        }
+    }
 
-    let name = document.createElement('h3')
-    name.classList.add('uk-card-title')
-    name.innerText = fileName
+    hideSpinner() {
+        let spinner = document.getElementById("spinner")
+        spinner.classList.add('uk-animation-reverse')
+        spinner.classList.add('uk-animation-fade')
+    }
 
-    card.append(icon)
-    card.append(name)
-    e.append(card)
-    return e
+    createPdfElement(fileName) {
+        let e = document.createElement('div')
+
+        let card = document.createElement('div')
+        card.classList.add('uk-card')
+        card.classList.add('uk-card-default')
+
+        let icon = document.createElement('span')
+        icon.classList.add('uk-card-body')
+        icon.setAttribute('uk-icon', 'icon: file-pdf; ratio: 8')
+
+        let name = document.createElement('h3')
+        name.classList.add('uk-card-title')
+        name.innerText = fileName
+
+        card.append(icon)
+        card.append(name)
+        e.append(card)
+        return e
+    }
 }
 
-for (let file of response) {
-    console.log(file)
-    fileGallery.append(createPdfElement(file['name']))
-}
-
+let controller = new ViewController()
